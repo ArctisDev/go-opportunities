d9cac6d54f335d0d0c745aa7c0b54be625aafcfe
Pedro Silva <mc33pedrobr@gmail.com>
2026-04-08 18:05:19 -0300

    docs: add documentation for CreateOpening, DeleteOpening, ListOpening, ShowOpening and UpdateOpening

--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ArctisDev/go-opportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,6 +20,36 @@ func sendSucess(ctx *gin.Context, op string, data interface{}) {
 	ctx.Header("Contet-type", "aplication/json")
 	ctx.JSON(http.StatusOK, gin.H{
 		"messsage": fmt.Sprintf("operation from handler: %s sucessfully executed", op),
-		"data":    data,
+		"data":     data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode int    `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type ListOpeningsResponse struct {
+	Message string                  `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
